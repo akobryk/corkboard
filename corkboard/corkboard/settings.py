0a9ebdad8291fea4e8ea31de06e5c3f47317ff45
Andrii Kobryk <a.kobryk@gmail.com>
2019-03-08 14:29:08 +0200

    Create board app

--- corkboard/corkboard/settings.py
+++ corkboard/corkboard/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'board',
+    
+    'rest_framework',
+    'taggit',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
